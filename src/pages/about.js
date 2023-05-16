3c36186d90fe7e8a8d6b9a96307545cd65902dea
Willi0t <william.sinclair92@gmail.com>
2023-05-16 14:00:48 +0200

    reverted changes, no change to render speed

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import AnimatedText from "@/components/AnimatedText";
 import Layout from "@/components/Layout";
 import Head from "next/head";
 import React, { useEffect, useRef } from "react";
-import profilePic from "../../public/images/profile/profile-pic.jpg";
+import profilePic from "../../public/images/profile/profile-pic.webp";
 import Image from "next/image";
 import { spring, useInView, useMotionValue, useSpring } from "framer-motion";
 import Skills from "@/components/Skills";
