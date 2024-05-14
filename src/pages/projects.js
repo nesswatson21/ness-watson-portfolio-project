9c625cdc5879b74bc6de99ddb51b0b5235c814ed
William <william.sinclair92@gmail.com>
2024-05-14 16:21:31 +0200

    changed featured products

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -11,182 +11,188 @@ import project2 from "../../public/images/projects/slack-mockup.png";
 import project3 from "../../public/images/projects/To-do-list-app.png";
 import project4 from "../../public/images/projects/my-first-page.png";
 import project5 from "../../public/images/projects/my-fighter.png";
-import project6 from "../../public/images/projects/weather-app.png";
+import project6 from "../../public/images/projects/E-commerce-front.jpg";
 
 const FramerImage = motion(Image);
 
 const FeaturedProject = ({ type, title, summary, img, link, githubLink }) => {
-  return (
-    <article className="w-full flex items-center justify-between relative rounded-br-2xl rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 lg:flex-col lg:p-8 lg:min-h-[336px] xs:rounded-2xl xs:rounded-br-3xl xs:p-4">
-      <div className="absolute top-0 -right-3 -z-10 w-[100.6%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]" />
-      <Link
-        href={link}
-        target="_blank"
-        className="w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full"
-      >
-        <FramerImage
-          src={img}
-          alt={title}
-          className="w-full h-auto"
-          whileHover={{ scale: 1.05 }}
-          transition={{ duration: 0.2 }}
-          priority
-        />
-      </Link>
-      <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
-        <span className="text-primary font-medium text-xl xs:text-base">
-          {type}
-        </span>
-        <Link
-          href={link}
-          target="_blank"
-          className="hover:underline underline-offset-2"
-        >
-          <h2 className="my-2 w-full text-left text-4xl font-bold sm:text-sm">
-            {title}
-          </h2>
-        </Link>
-        <p className="my-2 font-medium text-dark/75">{summary}</p>
-        <div className="mt-2 flex items-center">
-          <Link href={githubLink} target="_blank" className="w-10">
-            <GithubIcon />
-          </Link>
-          <Link
-            href={githubLink}
-            target="_blank"
-            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-smibold sm:px-4 sm:text-base"
-          >
-            Visit the project
-          </Link>
-        </div>
-      </div>
-    </article>
-  );
+    return (
+        <article className="w-full flex items-center justify-between relative rounded-br-2xl rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 lg:flex-col lg:p-8 lg:min-h-[336px] xs:rounded-2xl xs:rounded-br-3xl xs:p-4">
+            <div className="absolute top-0 -right-3 -z-10 w-[100.6%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]" />
+            <Link
+                href={link}
+                target="_blank"
+                className="w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full"
+            >
+                <FramerImage
+                    src={img}
+                    alt={title}
+                    className="w-full h-auto"
+                    whileHover={{ scale: 1.05 }}
+                    transition={{ duration: 0.2 }}
+                    priority
+                />
+            </Link>
+            <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
+                <span className="text-primary font-medium text-xl xs:text-base">
+                    {type}
+                </span>
+                <Link
+                    href={link}
+                    target="_blank"
+                    className="hover:underline underline-offset-2"
+                >
+                    <h2 className="my-2 w-full text-left text-4xl font-bold sm:text-sm">
+                        {title}
+                    </h2>
+                </Link>
+                <p className="my-2 font-medium text-dark/75">{summary}</p>
+                <div className="mt-2 flex items-center">
+                    <Link href={githubLink} target="_blank" className="w-10">
+                        <GithubIcon />
+                    </Link>
+                    <Link
+                        href={githubLink}
+                        target="_blank"
+                        className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-smibold sm:px-4 sm:text-base"
+                    >
+                        Visit the project
+                    </Link>
+                </div>
+            </div>
+        </article>
+    );
 };
 
 const Project = ({ type, title, summary, img, link, githubLink }) => {
-  return (
-    <article className="w-full  flex flex-col items-center justify-center rounded-2xl border border-soild border-dark bg-light p-6 relative md:p-4 xs:p-4">
-      <div className="absolute top-0 -right-3 -z-10 w-[101.3%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]" />
-      <Link
-        href={link}
-        target="_blank"
-        className="w-full cursor-pointer overflow-hidden rounded-lg"
-      >
-        <FramerImage
-          src={img}
-          alt={title}
-          className="w-full h-auto"
-          whileHover={{ scale: 1.05 }}
-          transition={{ duration: 0.2 }}
-        />
-      </Link>
-      <div className="w-full flex flex-col items-start justify-between mt-4  2xl:min-h-[172px] lg:min-h-[164px] md:min-h-[152px]">
-        <span className="text-primary font-medium text-xl lg:text-lg md:text-base">
-          {type}
-        </span>
-        <Link
-          href={link}
-          target="_blank"
-          className="hover:underline underline-offset-2"
-        >
-          <h2 className="my-2 w-full text-left text-3xl font-bold lg:text-2xl">
-            {title}
-          </h2>
-        </Link>
-        <p className="my-2 font-medium text-dark/75 sm:text-sm">{summary}</p>
-        <div className=" w-full mt-2 flex items-center justify-between">
-          <Link
-            href={link}
-            target="_blank"
-            className=" text-lg font-semibold underline md:text-base"
-          >
-            Visit
-          </Link>
-          <Link href={githubLink} target="_blank" className="w-8 md:w-6">
-            <GithubIcon />
-          </Link>
-        </div>
-      </div>
-    </article>
-  );
+    return (
+        <article className="w-full  flex flex-col items-center justify-center rounded-2xl border border-soild border-dark bg-light p-6 relative md:p-4 xs:p-4">
+            <div className="absolute top-0 -right-3 -z-10 w-[101.3%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]" />
+            <Link
+                href={link}
+                target="_blank"
+                className="w-full cursor-pointer overflow-hidden rounded-lg"
+            >
+                <FramerImage
+                    src={img}
+                    alt={title}
+                    className="w-full h-auto"
+                    whileHover={{ scale: 1.05 }}
+                    transition={{ duration: 0.2 }}
+                />
+            </Link>
+            <div className="w-full flex flex-col items-start justify-between mt-4  2xl:min-h-[172px] lg:min-h-[164px] md:min-h-[152px]">
+                <span className="text-primary font-medium text-xl lg:text-lg md:text-base">
+                    {type}
+                </span>
+                <Link
+                    href={link}
+                    target="_blank"
+                    className="hover:underline underline-offset-2"
+                >
+                    <h2 className="my-2 w-full text-left text-3xl font-bold lg:text-2xl">
+                        {title}
+                    </h2>
+                </Link>
+                <p className="my-2 font-medium text-dark/75 sm:text-sm">
+                    {summary}
+                </p>
+                <div className=" w-full mt-2 flex items-center justify-between">
+                    <Link
+                        href={link}
+                        target="_blank"
+                        className=" text-lg font-semibold underline md:text-base"
+                    >
+                        Visit
+                    </Link>
+                    <Link
+                        href={githubLink}
+                        target="_blank"
+                        className="w-8 md:w-6"
+                    >
+                        <GithubIcon />
+                    </Link>
+                </div>
+            </div>
+        </article>
+    );
 };
 
 const projects = () => {
-  return (
-    <>
-      <Head>
-        <title>william | Projects Page</title>
-        <meta name="description" content="any description" />
-      </Head>
-      <main className="w-full mb-16 flex flex-col items-center justify-center">
-        <Layout className="pt-16">
-          <AnimatedText
-            text="Hyper Island projects"
-            className=" mb-28 w-[90%] lg:!text-7xl sm:mb-8 sm:!text-4xl"
-          />
-          <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
-            <div className="col-span-12">
-              <FeaturedProject
-                title="My Portfolio"
-                img={project1}
-                summary="I wanted to create a site that showcased my skills and projects in a clean and modern way. With NextJS, I was able to build a fast and responsive site that highlights my work and experience. Take a look around and feel free to reach out if you have any questions!"
-                link="/https://darling-cupcake-0b1da6.netlify.app/projects"
-                type="Featured Project"
-                githubLink="https://github.com/Willi0t/my-portfolio"
-              />
-            </div>
-            <div className="col-span-6 sm:col-span-12">
-              <Project
-                title="Slack recreation"
-                img={project2}
-                link="https://mellifluous-sawine-7134b3.netlify.app/"
-                type="Project"
-                githubLink="https://github.com/Willi0t/slack2.0"
-              />
-            </div>
-            <div className="col-span-6 sm:col-span-12">
-              <Project
-                title="To-do list app with REST API"
-                img={project3}
-                link="https://todolist-fullstack.netlify.app/"
-                type="Project"
-                githubLink="https://github.com/app-HI/todolist-pernstack"
-              />
-            </div>
-            <div className="col-span-12">
-              <FeaturedProject
-                title="My very first webpage"
-                img={project4}
-                summary="this was my very first webpage. i wanted to showcase this becouse im still proud of the design i made by justing HTML and CSS."
-                link="https://bucolic-tarsier-4e2594.netlify.app/"
-                type="Featured Project"
-                githubLink="https://github.com/Willi0t/scandinavia"
-              />
-            </div>
-            <div className="col-span-6 sm:col-span-12">
-              <Project
-                title="arcade fighter with Hyper Island theme"
-                img={project5}
-                link="https://jovial-moonbeam-d7fc97.netlify.app/"
-                type="Project"
-                githubLink="https://github.com/munnizashah/HyperBattle"
-              />
-            </div>
-            <div className="col-span-6 sm:col-span-12">
-              <Project
-                title="Weather app"
-                img={project6}
-                link="https://illustrious-dolphin-aefc72.netlify.app/"
-                type="Project"
-                githubLink="https://github.com/Willi0t/weather"
-              />
-            </div>
-          </div>
-        </Layout>
-      </main>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>william | Projects Page</title>
+                <meta name="description" content="any description" />
+            </Head>
+            <main className="w-full mb-16 flex flex-col items-center justify-center">
+                <Layout className="pt-16">
+                    <AnimatedText
+                        text="Hyper Island projects"
+                        className=" mb-28 w-[90%] lg:!text-7xl sm:mb-8 sm:!text-4xl"
+                    />
+                    <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
+                        <div className="col-span-12">
+                            <FeaturedProject
+                                title="E-commerce platform"
+                                img={project6}
+                                summary="I created an e-commerce store using Next.js, featuring a user-friendly front end displaying products and a robust admin panel for managing inventory and orders. The store seamlessly integrates both functionalities to ensure a smooth shopping experience for customers and efficient management for administrators."
+                                link="/https://darling-cupcake-0b1da6.netlify.app/projects"
+                                type="Featured Project"
+                                githubLink="https://github.com/Willi0t/ecommerce-front"
+                            />
+                        </div>
+                        <div className="col-span-6 sm:col-span-12">
+                            <Project
+                                title="Slack recreation"
+                                img={project2}
+                                link="https://mellifluous-sawine-7134b3.netlify.app/"
+                                type="Project"
+                                githubLink="https://github.com/Willi0t/slack2.0"
+                            />
+                        </div>
+                        <div className="col-span-6 sm:col-span-12">
+                            <Project
+                                title="To-do list app with REST API"
+                                img={project3}
+                                link="https://todolist-fullstack.netlify.app/"
+                                type="Project"
+                                githubLink="https://github.com/app-HI/todolist-pernstack"
+                            />
+                        </div>
+                        <div className="col-span-12">
+                            <FeaturedProject
+                                title="My Portfolio"
+                                img={project1}
+                                summary="I wanted to create a site that showcased my skills and projects in a clean and modern way. With NextJS, I was able to build a fast and responsive site that highlights my work and experience. Take a look around and feel free to reach out if you have any questions!"
+                                link="/https://darling-cupcake-0b1da6.netlify.app/projects"
+                                type="Featured Project"
+                                githubLink="https://github.com/Willi0t/my-portfolio"
+                            />
+                        </div>
+                        <div className="col-span-6 sm:col-span-12">
+                            <Project
+                                title="arcade fighter with Hyper Island theme"
+                                img={project5}
+                                link="https://jovial-moonbeam-d7fc97.netlify.app/"
+                                type="Project"
+                                githubLink="https://github.com/munnizashah/HyperBattle"
+                            />
+                        </div>
+                        <div className="col-span-6 sm:col-span-12">
+                            <Project
+                                title="My very first webpage"
+                                img={project4}
+                                link="https://bucolic-tarsier-4e2594.netlify.app/"
+                                type="Featured Project"
+                                githubLink="https://github.com/Willi0t/scandinavia"
+                            />
+                        </div>
+                    </div>
+                </Layout>
+            </main>
+        </>
+    );
 };
 
 export default projects;
